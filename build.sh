4025215f2a2f203abc3d732bc1b40a3912ff661e
Vignesh Paranthaman <vignesh-p@outlook.com>
2021-06-21 15:25:36 +0530

    first commit

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Loading emsdk
+source $EMSDK/emsdk_env.sh
+export PATH=:$EMSDK/upstream/bin:$PATH
+
+cd ffmpeg
+
+# verify Emscripten version
+emcc -v
+
+# configure FFMpeg with Emscripten
+CFLAGS="-s USE_PTHREADS"
+LDFLAGS="$CFLAGS -s INITIAL_MEMORY=33554432" # 33554432 bytes = 32 MB
+CONFIG_ARGS=(
+  --target-os=none        # use none to prevent any os specific configurations
+  --arch=x86_32           # use x86_32 to achieve minimal architectural optimization
+  --enable-cross-compile  # enable cross compile
+  --disable-x86asm        # disable x86 asm
+  --disable-inline-asm    # disable inline asm
+  --disable-stripping     # disable stripping
+#  --disable-programs      # disable programs build (incl. ffplay, ffprobe & ffmpeg)
+  --disable-doc           # disable doc
+  --extra-cflags="$CFLAGS"
+  --extra-cxxflags="$CFLAGS"
+  --extra-ldflags="$LDFLAGS"
+  --nm="llvm-nm -g"
+  --ar=emar
+#  --as=llvm-as
+  --ranlib=llvm-ranlib
+  --cc=emcc
+  --cxx=em++
+  --objcc=emcc
+  --dep-cc=emcc
+)
+emconfigure ./configure "${CONFIG_ARGS[@]}"
+
+# build ffmpeg
+emmake make -j3
+
+# making dist
+mkdir -p ../dist
+
+# build ffmpeg.wasm
+cp ./ff*_g* ../dist/
+cd ../dist
